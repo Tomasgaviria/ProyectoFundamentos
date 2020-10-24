929a98e1760d37a8b2a27862ab02fbf00931650b
Tomás Gaviria <tgaviriao@eafit.edu.co>
2020-10-23 20:53:53 -0500

    Intercambio inicial de proyecto

---
+++ FriendZone.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class FriendZone{
+    public static void main (String[]args ){
+         Scanner scan = new Scanner(System.in);
+        String hombre = scan.next();
+        String mujer = scan.next();
+        System.out.println(hombre +" deja en la friend zone "+ mujer);
+    }
+}
