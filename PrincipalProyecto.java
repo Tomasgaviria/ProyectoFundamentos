742b15012eb8b61ba56a251f93accd7be7bde2c8
Tomás Gaviria <tgaviriao@eafit.edu.co>
2020-10-24 14:41:56 -0500

    Justifiación

--- PrincipalProyecto.java
+++ PrincipalProyecto.java
@@ -4,63 +4,63 @@ public class PrincipalProyecto{
     public static void main(String [] args){
         PrincipalProyecto.mostrarMenu();
     }
-    
+
     public static void mostrarMenu(){
         Scanner scan = new Scanner(System.in);
         while(true){
             System.out.println("Ingrese número:");
             int n = scan.nextInt();
-             if(n == 0){
+            if(n == 0){
                 break;
             }
-             else if(n == 1){
-                   if (Vehiculo.posAnadir<Vehiculo.TAMANO){
-                  System.out.println("Ingrese modelo:");
-                  int modelo = scan.nextInt();
-                  System.out.println("Ingrese marca:");
-                  String marca = scan.next();
-                  System.out.println("Ingrese valor comercial:");
-                  double valorComercial= scan.nextDouble();
-                  System.out.println("Ingrese color:");
-                  String color = scan.next();
-                  Vehiculo v1 = new Vehiculo(modelo, marca, valorComercial, color);
-              }else {System.out.println("Error base de datos llena");
-               }
+            else if(n == 1){
+                if (Vehiculo.posAnadir<Vehiculo.TAMANO){
+                    System.out.println("Ingrese modelo:");
+                    int modelo = scan.nextInt();
+                    System.out.println("Ingrese marca:");
+                    String marca = scan.next();
+                    System.out.println("Ingrese valor comercial:");
+                    double valorComercial= scan.nextDouble();
+                    System.out.println("Ingrese color:");
+                    String color = scan.next();
+                    Vehiculo v1 = new Vehiculo(modelo, marca, valorComercial, color);
+                }else {System.out.println("Error base de datos llena");
+                }
             }
-             else if(n == 2){
-                 System.out.println(Vehiculo.toStringVehiculos());
-             }
-             else if(n == 3){
-                 System.out.println(Vehiculo.cantidadVehiculos());
-             }
-             else if(n == 4){
+            else if(n == 2){
+                System.out.println(Vehiculo.toStringVehiculos());
+            }
+            else if(n == 3){
+                System.out.println(Vehiculo.cantidadVehiculos());
+            }
+            else if(n == 4){
                 System.out.println(Vehiculo.soloVerdes());
-             }
-             else if(n == 5){
+            }
+            else if(n == 5){
                 if(Sensor.posAñadir<Sensor.TAMAÑO){
-                   System.out.println("Ingrese tipo:");
-                   String tipo = scan.next();
-                   System.out.println("Ingrese valor comercial:");
-                   double valorComercial = scan.nextDouble();
-                   Sensor s1 = new Sensor (tipo, valorComercial);
+                    System.out.println("Ingrese tipo:");
+                    String tipo = scan.next();
+                    System.out.println("Ingrese valor comercial:");
+                    double valorComercial = scan.nextDouble();
+                    Sensor s1 = new Sensor (tipo, valorComercial);
                 }else{
                     System.out.println("Error base de datos llena");
                 }
-             } 
-             else if(n == 6){
+            } 
+            else if(n == 6){
                 System.out.println(Sensor.toStringSensores());
-             }
-             else if(n == 7){
-                System.out.println(Sensor.cantidadSensores());
-                }
-             else if(n == 8){
-                 System.out.println(Sensor.temperaturaSensores());
-                }
-             else if(n == 666){
-               System.out.println(Sensor.toStringOrden(Sensor.listaTemperaturas()));
-               }else{
-               break;
-             }
             }
+            else if(n == 7){
+                System.out.println(Sensor.cantidadSensores());
+            }
+            else if(n == 8){
+                System.out.println(Sensor.temperaturaSensores());
+            }
+            else if(n == 666){
+                System.out.println(Sensor.toStringOrden(Sensor.listaTemperaturas()));
+            }else{
+                break;
+            }
+        }
     }
 }
--- Vehiculo.java
+++ Vehiculo.java
@@ -1,20 +1,22 @@
- public class Vehiculo 
+public class Vehiculo 
 {
-    public static Vehiculo[] vehiculos  = new Vehiculo[10];;
+    public static Vehiculo[] vehiculos  = new Vehiculo[10];
     public static int posAnadir = 0;
     private int modelo;
     private String marca;
     private double valorComercial;
     private String color;
     public final static int TAMANO = 10;
-    
+
     public Vehiculo() {
         Vehiculo.vehiculos[Vehiculo.posAnadir] = this;
         Vehiculo.posAnadir++;
     }
+
     public Vehiculo(int modelo, String marca, double valorComercial) {
         this(modelo,marca,valorComercial,"verde");
     }
+
     public Vehiculo(int modelo, String marca, double valorComercial, String color) {
         this.modelo = modelo;
         this.marca = marca;
@@ -23,49 +25,60 @@
         Vehiculo.vehiculos[Vehiculo.posAnadir] = this;
         Vehiculo.posAnadir++;
     }
+
     public String toString() {
         String infoVehiculo = "* " + this.modelo + ", " + this.marca + ", " + this.valorComercial + ", " + this.color + " *";;
-         return infoVehiculo;
+        return infoVehiculo;
     }
+
     public static String toStringVehiculos() {
         String totalVehiculos="";
         for(int i=0;i<posAnadir;i++){
             totalVehiculos = totalVehiculos+vehiculos[i].toString();
-         }
-         if(totalVehiculos.equals("")){
-             totalVehiculos = "no hay vehiculos";
-            }
+        }
+        if(totalVehiculos.equals("")){
+            totalVehiculos = "no hay vehiculos";
+        }
         return totalVehiculos;
     }
+
     public static int cantidadVehiculos()
     {
         return posAnadir;
     }
+
     public int getModelo() {
         return modelo;
     }
+
     public void setModelo(int modelo) {
         this.modelo = modelo;
     }
+
     public String getMarca() {
         return marca;
     }
+
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
     public double getValorComercial() {
         return valorComercial;
     }
+
     public void setValorComercial(double valorComercial) {
         this.valorComercial = valorComercial;
-    }
+     }
+
     public String getColor() {
         return color;
-    }
+     }
+
     public void setColor(String color) {
         this.color = color;
-    }
-    
+     }
+
     public static String soloVerdes(){
         String losVerdes="";
         if(Vehiculo.posAnadir!=0){
